edc5bffd9d2770e23540f205c4b7421cf17928b5
Binaya Sharma <binaya.sharma@stackguardian.io>
2023-07-21 12:34:58 +0545

    Removing unwanted sections and variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "registry_name" {
-  description = "The name of this Container Registry."
+  description = "(Required) Specifies the name of the Container Registry. Changing this forces a new resource to be created."
 }
 
 variable "location" {
-  description = "The supported Azure location where the resources exist."
+  description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the resources."
+  description = "(Required) The name of the resource group in which to create the Container Registry. Changing this forces a new resource to be created."
   type        = string
 }
 
 variable "sku" {
-  description = "The SKU tier for the Container Registry."
+  description = "Optional) The SKU name of the container registry. Possible values are Classic (which was previously Basic), Basic, Standard and Premium."
   type        = string
   default     = "Basic"
 }
@@ -33,51 +33,4 @@ variable "georeplications" {
   }))
 
   default = []
-}
-
-variable "webhooks" {
-  description = "A map of webhooks to create for this Container Registry."
-
-  type = map(object({
-    name        = string
-    service_uri = string
-    actions     = optional(list(string), ["push"])
-    status      = optional(string, "enabled")
-    scope       = optional(string, "")
-  }))
-
-  default = {}
-}
-
-variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics workspace to send diagnostics to."
-  type        = string
-}
-
-variable "log_analytics_destination_type" {
-  description = "The type of log analytics destination to use for this Log Analytics Workspace."
-  type        = string
-  default     = null
-}
-
-variable "diagnostic_setting_enabled_log_categories" {
-  description = "A list of log categories to be enabled for this diagnostic setting."
-  type        = list(string)
-
-  default = [
-    "ContainerRegistryLoginEvents",
-    "ContainerRegistryRepositoryEvents"
-  ]
-}
-
-variable "diagnostic_setting_name" {
-  description = "The name of this Diagnostic Setting."
-  type        = string
-  default     = "audit-logs"
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
 }
